45ef771999052503ae825011527ee97711bc22a4
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-03-22 09:50:00 +0100

    Add command if used with proxy

--- get_pathogen.bat
+++ get_pathogen.bat
@@ -1,1 +1,4 @@
 @wget --no-check-certificate "https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim" -O autoload\pathogen.vim
+
+@rem with proxy
+@rem wget --no-check-certificate "https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim" -O autoload\pathogen.vim -e --use-proxy=on -e --https_proxy=%HTTP_PROXY%
